91d2dc994bfc8e5dac2fcf71be4c97adfeff8781
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-09-26 22:53:53 +0200

    Added main.js containg a simple timed main game loop

---
+++ main.js
@@ -0,0 +1,38 @@
+(function(){
+    /* some variables for time-keeping in the main loop */
+     var t = +new Date();
+     var dt = 0;
+     var old_time = t;
+
+     function setup(){
+        /* all setup code goes here */
+        console.log("setup");
+     }
+
+     function update(){
+         /* this is the update function that gets called 50 fps */
+         console.log("update");
+     }
+
+     /* our asynchrnous main game loop */
+     function loop(){
+
+         /* cue the loop for another round */
+         setTimeout(function(){loop();},0);
+
+         /* calculate time since last execution */
+         t = +new Date();
+         dt += (t-old_time);
+         old_time = t;
+
+         /* if enough time has passed, call update the apropriate amount of time */
+         while(dt>20){
+             update();
+             dt-= 20;
+         }
+     };
+
+     /* kickoff! */
+     setup();
+     loop();
+})();
