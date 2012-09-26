b55323f0ee35b0167a058e884c3d8f2d6c43a85f
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2012-09-27 01:34:35 +0200

    Added some fields to Player

--- Player.js
+++ Player.js
@@ -1,8 +1,15 @@
 module.exports = Player;
 function Player(){
+    this.x = 0;
+    this.y = 0;
+    this.dx = 0;
+    this.dy = 0;
+
+    /* TODO: find out how the joystick works, and model it here */
+    this.controllerState = {};
 }
 
 Player.prototype.update = function() {
-    // w/e
+    /* TODO: look at the controllerState, and act on it */
 }
 
